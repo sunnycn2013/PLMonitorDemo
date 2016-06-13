ed6b4d2bb0d3f1b07adf0071d49ed2da571770d4
上海－郭朝阳 <guochaoyang@jd.com>
2016-06-13 18:20:37 +0800

    init

--- PLMonitorDemo/AppDelegate.m
+++ PLMonitorDemo/AppDelegate.m
@@ -7,6 +7,7 @@
 //
 
 #import "AppDelegate.h"
+#import "SMLagMonitor.h"
 
 @interface AppDelegate ()
 
@@ -17,6 +18,7 @@
 
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions {
     // Override point for customization after application launch.
+    [[SMLagMonitor shareInstance] beginMonitor];
     return YES;
 }
 
---
+++ Podfile
@@ -0,0 +1,7 @@
+platform :ios, '7.0'
+
+target 'PLMonitorDemo' do
+	pod 'Masonry'
+	pod 'MJRefresh'
+    #pod 'Reveal-iOS-SDK'
+end
